ce7ec7e8dee9baed856e08019703c7d7b94e1379
PearOak <PearOak@users.noreply.github.com>
2020-05-14 11:31:37 -0300

    Prime number detector
    This shell can detect whether a number is prime or not

---
+++ primo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Please, input a number: "
+read NUM
+
+DIV=1
+CONT=0
+
+echo "\n\n-------------------- DOING THE MATH --------------------\n"
+while [[ $DIV -le $NUM ]]; do
+	RES=$[ $NUM / $DIV ]
+	REM=$[ $NUM % $DIV ]
+
+	if [[ $REM -eq 0 ]]; then
+		echo -e "${NUM}/${DIV} = ${RES}\nRemain: ${REM}"
+		CONT=$[ $CONT + 1 ]
+		echo "Total dividers (so far): ${CONT}"
+#	else
+#		echo -e "${NUM}/${DIV} = ${RES} \n Remain: ${REM}"
+#		echo "Total dividers (so far): ${CONT}"
+	fi
+
+	DIV=$[ $DIV + 1 ]
+done
+
+echo -e "\n\n------------------- CONCLUSION --------------------\n"
+
+if [[ $CONT -eq 2 ]]; then
+	echo "${NUM} is a prime number, since it is divided by ${CONT} numbers!"
+else
+	echo "${NUM} is not a prime number, sice it is divided by ${CONT} numbers!"
+fi
+
+echo "Done! :D"
