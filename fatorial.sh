93ef93a3a5e20fc3623bf0d7036d46c4e6f91e99
PearOak <PearOak@users.noreply.github.com>
2020-05-14 11:28:39 -0300

    Factorials
    This shell script calculates the factorial of any number.
    Obs: The script will work smootly until 20!. After that, the script will exceed C's unsigned long long int limit.

---
+++ fatorial.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Digite o número que se deseja descobrir o fatorial:"
+read NUMERO #Lê o numero que se deseja extrair o fatorial
+FAT=$NUMERO #Será apenas usado no fim
+RESULT=1 #Número utilizado como resultado
+
+while [[ $NUMERO -ge 0 ]]; do
+	if [[ $NUMERO -eq 0 ]]; then
+		break #Como fatorial de 0 é 1, então o looping vai ser quebrado ai
+	elif [[ $NUMERO -lt 0 ]]; then
+		echo "Não existe fatorial de número negativo, arrombado!" #Dispensa comentários, né? kkkk
+		break
+	else
+		RESULT=$(( $RESULT * $NUMERO )) #Operaçao do fatorial. Result mudará de valor em cada iteração
+		NUMERO=$(( $NUMERO - 1 )) #Número vai diminuir até 0
+	fi
+done
+
+echo "${FAT}! = ${RESULT}"
