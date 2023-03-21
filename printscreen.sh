2adec986a166cd10efae9fbdca35bfeeec158365
Wellington Pereira de Carvalho <PearOak@users.noreply.github.com>
2023-03-21 08:23:59 -0300

    Delete printscreen.sh

--- printscreen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir "/home/muttley/Desktop/Misc/Printscreens/$(date +'%d-%m-%Y')"
-import "/home/muttley/Desktop/Misc/Printscreens/$(date +'%d-%m-%Y')/Printscreen-$(date +'%d-%m-%Y-%H:%M:%S').png"
